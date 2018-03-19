0a84a8eb8f964b30ff365b693d9d93aeec7faf34
Lawliet <jerry_howl_lawliet@hotmail.com>
2018-03-19 17:38:51 -0600

    Código Comentado
    Se Comentó El Código Para Comprender El Funcionamiento del Algoritmo.

--- CheckSum.py
+++ CheckSum.py
@@ -1,26 +1,42 @@
 
+# CheckSum.py
+# By: LawlietJH
+
 def GetChecksum(Pin):
 	
-	pin = Pin * 10
-	acc = 0
+	Acc = 0									# Se Inicializa Un Acumulador.
 	
-	acc = acc + 3 * ((pin//10000000) % 10)
-	acc = acc + 1 * ((pin//1000000) % 10)
-	acc = acc + 3 * ((pin//100000) % 10)
-	acc = acc + 1 * ((pin//10000) % 10)
-	acc = acc + 3 * ((pin//1000) % 10)
-	acc = acc + 1 * ((pin//100) % 10)
-	acc = acc + 3 * ((pin//10) % 10)
-	digito = acc % 10
-	checksum = (10 - digito) % 10
+	if not Pin.isdigit() or not len(Pin) == 7: return 'Error'
+	else: Pin = int(Pin)
 	
-	return pin + checksum
+	Pin = Pin * 10							# Se Agrega un Cero, Octavo Digito.
+	
+	Acc = Acc + 3 * ((Pin//10000000) % 10)	# Extrae El Primer  Digito (De Izq. a Der.), Se Multiplica por 3 y Se Acumula.
+	Acc = Acc + 	((Pin//1000000) % 10)	# Extrae El Segundo Digito (De Izq. a Der.) y Se Acumula.
+	Acc = Acc + 3 * ((Pin//100000) % 10)	# Extrae El Tercer  Digito (De Izq. a Der.), Se Multiplica por 3 y Se Acumula.
+	Acc = Acc + 	((Pin//10000) % 10)		# Extrae El Cuarto  Digito (De Izq. a Der.) y Se Acumula.
+	Acc = Acc + 3 * ((Pin//1000) % 10)		# Extrae El Quinto  Digito (De Izq. a Der.), Se Multiplica por 3 y Se Acumula.
+	Acc = Acc + 	((Pin//100) % 10)		# Extrae El Sexto   Digito (De Izq. a Der.) y Se Acumula.
+	Acc = Acc + 3 * ((Pin//10) % 10)		# Extrae El Septimo Digito (De Izq. a Der.), Se Multiplica por 3 y Se Acumula.
+	
+	Digito = Acc % 10						# Se Extrae el Ultimo Digito del Valor Acumulado.
+	CheckSum = (10 - Digito) % 10			# Se Le Resta al Numero 10 el Digito, Si el Resultado Fuera 10 se toma como 0.
+	
+	return Pin + CheckSum					# Se Devuelve el PIN ya Completo.
 
-#      PIN     CHECKSUM
-# 2012822          7
-# 0938604          4
-# 1234567          0
-
-Checksum = GetChecksum(938604)
-
-print('\n\n PIN:', Checksum)
+if __name__ == "__main__":
+	
+	# Ejemplos Para Pruebas:
+	
+	#	Completo		  PIN     CHECKSUM		Verificación
+	#
+	#	20128227		2012822		 7			Correcto
+	#	09386044		0938604		 4			Correcto
+	#	12345670		1234567		 0			Correcto
+	#	01234565		0123456		 5			Correcto
+	#	12332112		1233211		 2			Incorrecto	(Checksum Correcto: 3)
+	
+	PIN = GetChecksum('2012822')
+	
+	print('\n\n PIN:', PIN)
+	
